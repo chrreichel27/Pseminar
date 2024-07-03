e2d38c512bfb569baa51beb1891030f39fc36684
chrreichel27 <150839586+chrreichel27@users.noreply.github.com>
2024-07-03 14:26:28 +0200

    Delete ProjektPSeminar directory

--- ProjektPSeminar/src/utilz/LoadSave.java
+++
@@ -1,9 +0,0 @@
-package utilz;
-
-import java.awt.Color;
-
-public class LoadSave {
-
-    public static final String PLAYER_ATLAS = "Knight_Sprites_2.0.png";
-    public static final String GHOST_SPRITE = "Ghost.png";
-}
